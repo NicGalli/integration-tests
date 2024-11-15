35fdbd2a0ba24fb87cfb839a301ba8d31255f78a
NicGalli <nikk094@live.it>
2024-11-15 17:42:19 +0100

    added everything

--- src/main/java/com/example/school/view/swing/StudentSwingView.java
+++ src/main/java/com/example/school/view/swing/StudentSwingView.java
@@ -14,12 +14,20 @@ import javax.swing.border.EmptyBorder;
 
 import com.example.school.model.Student;
 import com.example.school.view.StudentView;
+import javax.swing.JButton;
+import javax.swing.JList;
 
-public class StudentSwingView extends JFrame implements StudentView{
+public class StudentSwingView extends JFrame implements StudentView {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtIdtextbox;
+	private JLabel lblName;
+	private JTextField textField;
+	private JButton btnAdd;
+	private JList list;
+	private JButton btnDeleteSelected;
+	private JLabel label;
 
 	/**
 	 * Launch the application.
@@ -49,52 +57,101 @@ public class StudentSwingView extends JFrame implements StudentView{
 
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[] {159, 12, 114, 0};
-		gbl_contentPane.rowHeights = new int[] {21, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] { 159, 12, 114, 0 };
+		gbl_contentPane.rowHeights = new int[] { 21, 0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		JLabel lblId = new JLabel("id");
 		GridBagConstraints gbc_lblId = new GridBagConstraints();
-		gbc_lblId.anchor = GridBagConstraints.SOUTHWEST;
-		gbc_lblId.insets = new Insets(0, 0, 0, 5);
-		gbc_lblId.gridx = 1;
-		gbc_lblId.gridy = 0;
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 1;
 		contentPane.add(lblId, gbc_lblId);
-		
+
 		txtIdtextbox = new JTextField();
 		txtIdtextbox.setName("idTextBox");
 		GridBagConstraints gbc_txtIdtextbox = new GridBagConstraints();
-		gbc_txtIdtextbox.anchor = GridBagConstraints.NORTHWEST;
-		gbc_txtIdtextbox.gridx = 2;
-		gbc_txtIdtextbox.gridy = 0;
+		gbc_txtIdtextbox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtIdtextbox.insets = new Insets(0, 0, 5, 5);
+		gbc_txtIdtextbox.gridx = 1;
+		gbc_txtIdtextbox.gridy = 1;
 		contentPane.add(txtIdtextbox, gbc_txtIdtextbox);
 		txtIdtextbox.setColumns(10);
+
+		lblName = new JLabel("name");
+		GridBagConstraints gbc_lblName = new GridBagConstraints();
+		gbc_lblName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblName.gridx = 0;
+		gbc_lblName.gridy = 2;
+		contentPane.add(lblName, gbc_lblName);
+
+		textField = new JTextField();
+		textField.setName("nameTextBox");
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 2;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		btnAdd = new JButton("Add");
+		btnAdd.setEnabled(false);
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAdd.gridx = 0;
+		gbc_btnAdd.gridy = 3;
+		contentPane.add(btnAdd, gbc_btnAdd);
+		
+		btnDeleteSelected = new JButton("Delete Selected");
+		GridBagConstraints gbc_btnDeleteSelected = new GridBagConstraints();
+		gbc_btnDeleteSelected.insets = new Insets(0, 0, 5, 5);
+		gbc_btnDeleteSelected.gridx = 1;
+		gbc_btnDeleteSelected.gridy = 3;
+		contentPane.add(btnDeleteSelected, gbc_btnDeleteSelected);
+		
+		label = new JLabel(" ");
+		label.setName("errorMessageLabel");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.insets = new Insets(0, 0, 5, 0);
+		gbc_label.gridx = 2;
+		gbc_label.gridy = 3;
+		contentPane.add(label, gbc_label);
+		
+		list = new JList();
+		list.setName("studentList");
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.insets = new Insets(0, 0, 0, 5);
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 1;
+		gbc_list.gridy = 4;
+		contentPane.add(list, gbc_list);
 	}
 
 	@Override
 	public void showAllStudents(List<Student> students) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void showError(String message, Student student) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void studentAdded(Student student) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void studentRemoved(Student student) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
