a1b1e3123d4be9ede65b71c8af3b99152c02207b
NicGalli <nikk094@live.it>
2025-09-23 20:10:02 +0200

    back to guice learning tests2

--- src/main/java/com/example/school/guice/SchoolControllerFactory.java
+++
@@ -1,9 +0,0 @@
-package com.example.school.guice;
-
-import com.example.school.controller.SchoolController;
-import com.example.school.view.StudentView;
-
-public interface SchoolControllerFactory {
-	
-	SchoolController create(StudentView view);
-}
